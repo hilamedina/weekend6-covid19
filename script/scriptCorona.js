d9b02ac66c2b011ca772f541bd0787a70c2d4740
hilamedina <hm18986@gmail.com>
2021-12-13 19:14:02 +0200

    covid

--- script/scriptCorona.js
+++ script/scriptCorona.js
@@ -1,17 +1,22 @@
+//   .getContext('2d');
 // const chartContinent = document
 //   .getElementById('#chartContinent')
-//   .getContext('2d');
 
 const graph = document.getElementById('chartContinent').getContext('2d');
 
-const Asiabutton = document.querySelector('.Asiabutton');
-const Americabutton = document.querySelector('.Americabutton');
-const Europebutton = document.querySelector('.Europebutton');
-const Africabutton = document.querySelector('.Africabutton');
-const worldbutton = document.querySelector('.worldbutton');
-const buttonconti = document.querySelector('.buttonconti');
+// const Asiabutton = document.querySelector('.Asiabutton');
+// const Americabutton = document.querySelector('.Americabutton');
+// const Europebutton = document.querySelector('.Europebutton');
+// const Africabutton = document.querySelector('.Africabutton');
+// const worldbutton = document.querySelector('.worldbutton');
+const buttonConti = document.querySelector('.buttonConti');
 const contiDiv2 = document.querySelector('.contiDiv2');
 const contiDiv1 = document.querySelector('.contiDiv1');
+const asiaButton = document.querySelector('.asiaButton');
+const europeButton = document.querySelector('.europeButton');
+const oceaniaButton = document.querySelector('.oceaniaButton');
+const americaButton = document.querySelector('.americaButton');
+const africaButton = document.querySelector('.africaButton');
 
 let chartContinent = new Chart(graph, {});
 
@@ -20,13 +25,42 @@ let chartContinent = new Chart(graph, {});
 // });
 
 let arrEurope = [];
+let europeConfirmed = [];
+let europeName = [];
+let europeDeaths = [];
+let europeRecoverd = [];
+let europeCriticalCondition = [];
+
 let arrAfrica = [];
-let arrAsia = [];
-let arrOceania = [];
+let africaConfirmed = [];
+let africaName = [];
+let africaDeaths = [];
+let africaRecoverd = [];
+let africaCriticalCondition = [];
+
 let arrAmerica = [];
-let restOfTheWorld = [];
-let asiaConfirmed = [];
+let americaConfirmed = [];
+let americaName = [];
+let americaDeaths = [];
+let americaRecoverd = [];
+let americaCriticalCondition = [];
+
+let arrAsia = [];
+let asia = [];
 let asiaName = [];
+let asiaConfirmed = [];
+let asiaRecoverd = [];
+let asiaDeaths = [];
+let asiaCriticalCondition = [];
+
+let arrOceania = [];
+let oceaniaConfirmed = [];
+let oceaniaName = [];
+let oceaniaDeaths = [];
+let oceaniaRecoverd = [];
+let oceaniaCriticalCondition = [];
+
+let restOfTheWorld = [];
 
 const urlCountries =
   'https://intense-mesa-62220.herokuapp.com/https://corona-api.com/countries';
@@ -39,9 +73,9 @@ async function getCountry() {
       name: item.name,
       code: item.code,
       ConfirmedCases: item.latest_data.confirmed,
-      NumberofDeaths: item.latest_data.deaths,
-      NumberOfRecovered: item.latest_data.recovered,
-      NumberOfCriticalCondition: item.latest_data.critical,
+      numberofDeaths: item.latest_data.deaths,
+      numberOfRecovered: item.latest_data.recovered,
+      numberOfCriticalCondition: item.latest_data.critical,
     });
   }
 
@@ -82,19 +116,41 @@ function newArrOfRegion(allCountriesArr) {
         arrAsia.push(state);
         asiaConfirmed.push(state.ConfirmedCases);
         asiaName.push(state.name);
-        // console.log(asiaName);
+        asiaDeaths.push(state.numberofDeaths);
+        asiaRecoverd.push(state.numberOfRecovered);
+        asiaCriticalCondition.push(state.numberOfCriticalCondition);
         break;
       case 'Europe':
         arrEurope.push(state);
+        europeConfirmed.push(state.ConfirmedCases);
+        europeName.push(state.name);
+        europeDeaths.push(state.numberofDeaths);
+        europeRecoverd.push(state.numberOfRecovered);
+        europeCriticalCondition.push(state.numberOfCriticalCondition);
         break;
       case 'America':
         arrAmerica.push(state);
+        americaConfirmed.push(state.ConfirmedCases);
+        americaName.push(state.name);
+        americaDeaths.push(state.numberofDeaths);
+        americaRecoverd.push(state.numberOfRecovered);
+        americaCriticalCondition.push(state.numberOfCriticalCondition);
         break;
       case 'Africa':
         arrAfrica.push(state);
+        africaConfirmed.push(state.ConfirmedCases);
+        africaName.push(state.name);
+        africaDeaths.push(state.numberofDeaths);
+        africaRecoverd.push(state.numberOfRecovered);
+        africaCriticalCondition.push(state.numberOfCriticalCondition);
         break;
       case 'Oceania':
         arrOceania.push(state);
+        oceaniaConfirmed.push(state.ConfirmedCases);
+        oceaniaName.push(state.name);
+        oceaniaDeaths.push(state.numberofDeaths);
+        oceaniaRecoverd.push(state.numberOfRecovered);
+        oceaniaCriticalCondition.push(state.numberOfCriticalCondition);
         break;
       default:
         restOfTheWorld.push(state);
@@ -125,26 +181,28 @@ async function showStat() {
   // const arrOfConti = ['Asia', 'Africa', 'Europe', 'America'];
   // const buttons = document.getElementsByClassName('buttonConti');
   // for (button in buttons) {
-  contiDiv2.addEventListener('click', () => {
+
+  asiaButton.addEventListener('click', () => {
     chartContinent.destroy();
     drawChart(asiaConfirmed, asiaName);
   });
+  europeButton.addEventListener('click', () => {
+    chartContinent.destroy();
+    drawChart(europeConfirmed, europeName);
+  });
+  africaButton.addEventListener('click', () => {
+    chartContinent.destroy();
+    drawChart(africaConfirmed, africaName);
+  });
+  oceaniaButton.addEventListener('click', () => {
+    chartContinent.destroy();
+    drawChart(oceaniaConfirmed, oceaniaName);
+  });
+  americaButton.addEventListener('click', () => {
+    chartContinent.destroy();
+    drawChart(americaConfirmed, americaName);
+  });
 }
-asiaButton.addEventListener('click', (e) => {
-  charted(asia, asiaConfirmed);
-});
-europeButton.addEventListener('click', () => {
-  charted(europe);
-});
-africaButton.addEventListener('click', () => {
-  charted(africa);
-});
-oceaniaButton.addEventListener('click', () => {
-  charted(oceania);
-});
-americasButton.addEventListener('click', () => {
-  charted(americas);
-});
 
 function drawChart(covidData, continent) {
   chartContinent = new Chart(graph, {
@@ -174,6 +232,7 @@ async function main() {
   allCountriesArr = await getConti(allCountriesArr);
   allCountriesArr = newArrOfRegion(allCountriesArr);
   await showStat();
+
   // console.log(allCountriesArr);
   // await graphfunction(arrEurope);
   // console.log(arrEurope);
